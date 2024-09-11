e40d2a6ef5b5dade26769e16eeb1182943f32213
Jaume Barcelo <jaume.barcelo@gmail.com>
2024-09-11 23:05:55 +0200

    adds seeders and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
 use App\Models\Category;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,15 +18,17 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CategorySeeder::class);
     
-
+        $AdminUser = new User();
+        $AdminUser->name = "admin";
+        $AdminUser->email = "admin@example.com";
+        $AdminUser->role = "role";
+        $AdminUser->password = Hash::make('password');
+        $AdminUser->save();
         
 
-        // User::factory(10)->create();
-/*
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-        */
+        User::factory(2)->create();
+
+        Post::factory(2)->create();
+
     }
 }
