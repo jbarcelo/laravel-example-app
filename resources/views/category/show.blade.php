a6d2f925e0f64bc957ccd1fef653675e0d853c9c
Jaume Barcelo <jaume.barcelo@gmail.com>
2024-09-17 22:47:35 +0200

    fixes/refactors category index, removes old forgotten code

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -15,17 +15,3 @@
         </div>
     </div>
 </x-app-layout>
-
-
-<table>
-    <tr  >
-        <td>{{ $category->id }}</td>
-        <td>{{ $category->title }}</td>
-        <td>{{ $category->url_clean }}</td>
-        <td>{{ $category->start_date }}</td>
-        <td>{{ $category->end_date }}</td>
-        <td>{{ $category->created_at }}</td>
-        <td>{{ $category->updated_at }}</td>
-        <td></td>
-    </tr>
-</table>
