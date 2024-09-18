228c2c5953cf84d0abd5207088931c05e7f22626
Jaume Barcelo <jaume.barcelo@gmail.com>
2024-09-18 22:05:44 +0200

    adds api/post/{post}

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -28,9 +28,9 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        //
+        return response()->json($post);
     }
 
     /**
