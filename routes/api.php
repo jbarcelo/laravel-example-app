ddd6b22a5cff07bec9e81893904a5902e8f0017e
Jaume Barcelo <jaume.barcelo@gmail.com>
2024-09-18 21:57:49 +0200

    adds /api/post

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('post', App\Http\Controllers\Api\PostController::class);
