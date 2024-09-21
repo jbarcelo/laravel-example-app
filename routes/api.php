c426405f06477938ab52b700ce029fccdc722ed3
Jaume Barcelo <jaume.barcelo@gmail.com>
2024-09-21 18:06:34 +0200

    adds authentication to API

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('post', PostController::class)->only(['index', 'show', 'store']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('post', PostController::class)->only(['index', 'show', 'store']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
